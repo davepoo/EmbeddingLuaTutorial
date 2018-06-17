f0ce6e6c3205a616c2c835e364ca747a304c31b0
James Allcock <davepoo@hotmail.com>
2018-06-17 12:29:37 +0100

    calling lua and native functions

--- main/main.cpp
+++ main/main.cpp
@@ -55,4 +55,70 @@ int main()
 
 		lua_close(L);
 	}
+
+	{
+		constexpr char* LUA_FILE = R"(
+		function Pythagoras( a, b )
+			return (a * a) + (b * b), a, b
+		end
+		)";
+
+		lua_State* L = luaL_newstate();
+		luaL_dostring(L, LUA_FILE);
+		lua_getglobal(L, "Pythagoras");
+		if (lua_isfunction(L, -1))
+		{
+			lua_pushnumber(L, 3);
+			lua_pushnumber(L, 4);
+			constexpr int NUM_ARGS = 2;
+			constexpr int NUM_RETURNS = 3;
+			lua_pcall(L, NUM_ARGS, NUM_RETURNS, 0);
+			lua_Number c = lua_tonumber(L, -3);
+			printf("csqr = %d\n", (int)c);
+			lua_Number a = lua_tonumber(L, -2);
+			printf("a = %d\n", (int)a);
+			lua_Number b = lua_tonumber(L, -1);
+			printf("b = %d\n", (int)b);
+		}
+		lua_close(L);
+	}
+
+	{
+		auto NativePythagoras = [](lua_State* L) -> int
+		{
+			lua_Number a = lua_tonumber(L, -2);
+			lua_Number b = lua_tonumber(L, -1);
+			lua_Number csqr = (a * a) + (b * b);
+			lua_pushnumber(L, csqr);
+			return 1;
+		};
+
+		constexpr char* LUA_FILE = R"(
+		function Pythagoras( a, b )
+			csqr = NativePythagoras( a, b )
+			return csqr, a, b
+		end
+		)";
+
+		lua_State* L = luaL_newstate();
+		lua_pushcfunction(L, NativePythagoras);
+		lua_setglobal(L, "NativePythagoras");
+		luaL_dostring(L, LUA_FILE);
+		lua_getglobal(L, "Pythagoras");
+		if (lua_isfunction(L, -1))
+		{
+			lua_pushnumber(L, 3);
+			lua_pushnumber(L, 4);
+			constexpr int NUM_ARGS = 2;
+			constexpr int NUM_RETURNS = 3;
+			lua_pcall(L, NUM_ARGS, NUM_RETURNS, 0);
+			lua_Number c = lua_tonumber(L, -3);
+			printf("csqr = %d\n", (int)c);
+			lua_Number a = lua_tonumber(L, -2);
+			printf("a = %d\n", (int)a);
+			lua_Number b = lua_tonumber(L, -1);
+			printf("b = %d\n", (int)b);
+		}
+		lua_close(L);
+	}
 }
