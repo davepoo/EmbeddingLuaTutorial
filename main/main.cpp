50b9d5d93d89022b5824469b936b92a3dc88227f
davep <davepoo@hotmail.com>
2018-06-30 14:45:35 +0100

    #16 Arena Allocator https://youtu.be/hiLfqyzr-nM

--- main/main.cpp
+++ main/main.cpp
@@ -1018,4 +1018,76 @@ int main()
 		assert(L != nullptr);
 		lua_close(L);
 	}
+
+	printf("---- lua arena memory allocation -----\n");
+	{
+		struct ArenaAllocator
+		{
+			void* m_begin;
+			void* m_end;
+			char* m_curr;
+
+			ArenaAllocator(void* begin, void* end) :
+				m_begin(begin),
+				m_end(end),
+				m_curr( static_cast<char*>(begin) )
+			{
+			}
+
+			void* Allocate(size_t sizeBytes)
+			{
+				assert(m_curr + sizeBytes < m_end); //we have run out of memory!!
+				printf("Allocated %d bytes\n", (int)sizeBytes);
+				void* ptr = m_curr;
+				m_curr += sizeBytes;
+				return ptr;
+			}
+
+			void DeAllocate(void* ptr, size_t osize )
+			{
+				assert(ptr != nullptr);		//can't decallocate null!!!
+				printf("DeAllocated %d bytes\n", (int)osize);
+				// we are just buring through memory.
+			}
+
+			void* ReAllocate(void* ptr, size_t osize, size_t nsize)
+			{
+				printf("ReAllocated %d bytes\n", (int)nsize);
+				void* newPtr = Allocate(nsize);
+				memcpy(newPtr, ptr, osize);
+				DeAllocate(ptr, osize);
+				return newPtr;
+			}
+
+			static void *l_alloc(void *ud, void *ptr, size_t osize, size_t nsize) {
+				ArenaAllocator * pool = static_cast<ArenaAllocator *>(ud);
+				if (nsize == 0) 
+				{
+					if (ptr != nullptr)
+					{
+						pool->DeAllocate(ptr, osize);
+					}
+					return NULL;
+				}
+				else
+				{
+					if (ptr == nullptr)
+					{
+						return pool->Allocate(nsize);
+					}
+					else
+					{
+						return pool->ReAllocate(ptr, osize, nsize);
+					}
+				}
+			}
+		};
+
+		constexpr int POOL_SIZE = 1024 * 10;
+		char memory[POOL_SIZE];
+		ArenaAllocator pool(memory, &memory[POOL_SIZE-1] );
+		lua_State* L = lua_newstate(ArenaAllocator::l_alloc, &pool);
+		assert(L != nullptr);
+		lua_close(L);
+	}
 }
