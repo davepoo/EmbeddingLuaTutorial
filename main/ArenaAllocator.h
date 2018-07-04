474ec5ba4e3c6d1e7d8521a739a9cf77a8455179
davep <davepoo@hotmail.com>
2018-07-04 21:29:02 +0100

    #19  Fallback Memory Allocator https://youtu.be/Fwkjlam-am4

--- main/ArenaAllocator.h
+++ main/ArenaAllocator.h
@@ -3,11 +3,62 @@
 #include <cstdio>
 #include <string.h>
 
+/*! \brief Allocates from global memory (NOTE: does not currently align memory) */
+struct GlobalAllocator
+{
+	void* Allocate(size_t sizeBytes)
+	{
+		return ::operator new(sizeBytes);
+	}
+
+	void DeAllocate(void* ptr, size_t /*osize*/)
+	{
+		assert(ptr != nullptr);		//can't decallocate null!!!
+		::operator delete(ptr);
+	}
+
+	void* ReAllocate(void* ptr, size_t osize, size_t nsize)
+	{
+		size_t bytesToCopy = osize;
+		if (nsize < bytesToCopy)
+		{
+			bytesToCopy = nsize;
+		}
+		void* newPtr = Allocate(nsize);
+		memcpy(newPtr, ptr, bytesToCopy);
+		DeAllocate(ptr, osize);
+		return newPtr;
+	}
+
+	static void *l_alloc(void *ud, void *ptr, size_t osize, size_t nsize) {
+		GlobalAllocator * pool = static_cast<GlobalAllocator *>(ud);
+		if (nsize == 0)
+		{
+			if (ptr != nullptr)
+			{
+				pool->DeAllocate(ptr, osize);
+			}
+			return NULL;
+		}
+		else
+		{
+			if (ptr == nullptr)
+			{
+				return pool->Allocate(nsize);
+			}
+			else
+			{
+				return pool->ReAllocate(ptr, osize, nsize);
+			}
+		}
+	}
+};
+
 /*! \brief Allocates from a fixed pool.
 *	Aligns all memory to 8 bytes
 *	Has a min allocation of 64 bytes
 *	Puts all free'd blocks on a free list.
-*	Asserts when out of memory. */
+*	Falls back to GlobalAllocator when out of memory. */
 struct ArenaAllocator
 {
 	void* m_begin;
@@ -23,6 +74,7 @@ struct ArenaAllocator
 	};
 
 	FreeList* m_freeListHead;
+	GlobalAllocator m_globalAllocator;
 
 	ArenaAllocator(void* begin, void* end) :
 		m_begin(begin),
@@ -60,37 +112,51 @@ struct ArenaAllocator
 		else
 		{
 			m_curr = (char*)((uintptr_t)m_curr + (ALIGNMENT - 1) & ~(ALIGNMENT - 1));
-			assert(m_curr + allocatedBytes < m_end); //we have run out of memory!!
-			//printf("Allocated %d bytes\n", (int)allocatedBytes);
-			void* ptr = m_curr;
-			m_curr += allocatedBytes;
-			return ptr;
+			if (m_curr + allocatedBytes <= m_end)
+			{
+				//printf("Allocated %d bytes\n", (int)allocatedBytes);
+				void* ptr = m_curr;
+				m_curr += allocatedBytes;
+				return ptr;
+			}
+			else
+			{
+				return m_globalAllocator.Allocate(sizeBytes);
+			}
 		}
 	}
 
 	void DeAllocate(void* ptr, size_t osize)
 	{
 		assert(ptr != nullptr);		//can't decallocate null!!!
-		size_t allocatedBytes = SizeToAllocate(osize);
-		//printf("DeAllocated %d bytes\n", (int)allocatedBytes);
-		if (allocatedBytes >= MIN_BLOCK_SIZE)
+		if (ptr >= m_begin && ptr <= m_end)
 		{
-			//printf("-- deallocated to the freelist --\n");
-			FreeList* newHead = static_cast<FreeList*>( ptr );
-			newHead->m_next = m_freeListHead;
-			m_freeListHead = newHead;
+			size_t allocatedBytes = SizeToAllocate(osize);
+			//printf("DeAllocated %d bytes\n", (int)allocatedBytes);
+			if (allocatedBytes >= MIN_BLOCK_SIZE)
+			{
+				//printf("-- deallocated to the freelist --\n");
+				FreeList* newHead = static_cast<FreeList*>(ptr);
+				newHead->m_next = m_freeListHead;
+				m_freeListHead = newHead;
+			}
 		}
 		else
 		{
-			// we are just buring through memory.
+			m_globalAllocator.DeAllocate(ptr, osize);
 		}
 	}
 
 	void* ReAllocate(void* ptr, size_t osize, size_t nsize)
 	{
 		//printf("ReAllocated %d bytes\n", (int)nsize);
+		size_t bytesToCopy = osize;
+		if (nsize < bytesToCopy)
+		{
+			bytesToCopy = nsize;
+		}
 		void* newPtr = Allocate(nsize);
-		memcpy(newPtr, ptr, osize);
+		memcpy(newPtr, ptr, bytesToCopy);
 		DeAllocate(ptr, osize);
 		return newPtr;
 	}
--- main/main.cpp
+++ main/main.cpp
@@ -950,22 +950,24 @@ int main()
 		constexpr char* LUA_FILE = R"(
 		sprite = Sprite.new()
 		sprite:Move( 6, 7 )		-- Sprite.Move( sprite, 6, 7 )
-		--sprite:Draw()
+		sprite:Draw()
 		sprite.y = 10
 		sprite.zzz = 99
 		sprite.x = sprite.zzz
 		temp_x = sprite.x
-		--sprite:Draw()
+		sprite:Draw()
 		)";
 
-		constexpr int POOL_SIZE = 1024 * 20;
+		constexpr int POOL_SIZE = 1024 * 10;
 		char memory[POOL_SIZE];
 		ArenaAllocator pool(memory, &memory[POOL_SIZE - 1]);
+		//GlobalAllocator pool;
 		//for (int i = 0; i < 50000; i++)	//comment this line in for benchmarking the mem allocator
 		{
 			pool.Reset();
 			lua_State* L = lua_newstate(ArenaAllocator::l_alloc, &pool);
 			//lua_State* L = luaL_newstate();
+			//lua_State* L = lua_newstate(GlobalAllocator::l_alloc, &pool);
 
 			lua_newtable(L);
 			int spriteTableIdx = lua_gettop(L);
