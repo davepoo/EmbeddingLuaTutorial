d182e2a76d8c57db7cd99aaca73bcadf3239d34a
davep <davepoo@hotmail.com>
2018-07-10 23:16:58 +0100

    #23 https://youtu.be/mwt0pOgFZFQ Calling Native Functions Using Run Time Type Information

--- main/TestRegistrations.cpp
+++ main/TestRegistrations.cpp
@@ -1,3 +1,29 @@
 #include <rttr/registration>
+#include <cstdio>
 
-// This file contains the stuff we are going to Register with RTTR and will be bound to Lua
+// This Cpp file contains the stuff we are going to 
+//Register with RTTR and will be bound to Lua.
+//NOTE: this file nor any of the contents does not need to 
+//be directly included/referenced in our Lua bindings.
+
+void HelloWorld()
+{
+	printf("Hello, World\n");
+}
+
+void HelloWorld2()
+{
+	printf("Hello, World 2\n");
+}
+
+void HelloWorld3()
+{
+	printf("Hello, World 3\n");
+}
+
+RTTR_REGISTRATION
+{
+	rttr::registration::method("HelloWorld", &HelloWorld);
+	rttr::registration::method("HelloWorld2", &HelloWorld2);
+	rttr::registration::method("HelloWorld3", &HelloWorld3);
+}
