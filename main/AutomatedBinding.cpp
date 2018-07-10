7c7b839cd2ffbd6dff2bda59af350387334afa0c
davep <davepoo@hotmail.com>
2018-07-10 21:00:01 +0100

    reverting changes back to default script

--- main/AutomatedBinding.cpp
+++ main/AutomatedBinding.cpp
@@ -3,23 +3,6 @@
 #include "lua.hpp"
 #include <cstdio>
 #include <assert.h>
-#include <rttr/registration>
-
-
-/*! \brief The Lua script, you would probably load this data from a .lua file. */
-constexpr char* LUA_SCRIPT = R"(
-		-- this is a lua script
-		Global.HelloWorld()
-		Global.HelloWorld2()
-		)";
-
-int CallGlobalFromLua(lua_State* L)
-{
-	rttr::method* m = (rttr::method*)lua_touserdata(L, lua_upvalueindex(1));
-	rttr::method& methodToInvoke(*m);
-	methodToInvoke.invoke({});
-	return 0;
-}
 
 void AutomatedBindingTutorial()
 {
@@ -33,21 +16,10 @@ void AutomatedBindingTutorial()
 	//open the Lua state using our memory pool
 	lua_State* L = lua_newstate(ArenaAllocator::l_alloc, &pool);
 
-	//bind our registered global methods to Lua
-	{
-		lua_newtable(L);
-		lua_pushvalue(L, -1);
-		lua_setglobal(L, "Global");
-
-		lua_pushvalue(L, -1);											//1							
-		for (auto& method : rttr::type::get_global_methods())
-		{
-			lua_pushstring(L, method.get_name().to_string().c_str());	//2
-			lua_pushlightuserdata(L, (void*)&method);
-			lua_pushcclosure(L, CallGlobalFromLua, 1);					//3
-			lua_settable(L, -3);										//1[2] = 3
-		}
-	}
+	/*! \brief The Lua script, you would probably load this data from a .lua file. */
+	constexpr char* LUA_SCRIPT = R"(
+		-- this is a lua script
+		)";
 
 	//execute the lua script
 	int doResult = luaL_dostring(L, LUA_SCRIPT);
